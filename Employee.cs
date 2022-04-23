ffb1b255b9175239633dd7956d6263611894acc7
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-23 19:23:02 +0530

    Connected to database

---
+++ Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmployeePayrollService
+{
+    public class Employee
+    {
+        public string Emp_Id { get; set; }
+        public string Emp_Name { get; set; }
+        public string Salary { get; set; }
+        public DateTime Joining_Date { get; set; }
+        public string Gender { get; set; }
+        public string Department { get; set; }
+        public string Address { get; set; }
+        public int Phone_number { get; set; }
+        public int Deductions { get; set; }
+        public int Taxable_Pay { get; set; }
+        public int Income_Tax { get; set; }
+        public int Net_Pay { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeePayrollService
+{   
+    public class EmployeePayroll
+    {
+        static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = Payroll; Integrated Security=SSPI;";
+        static SqlConnection connection=new SqlConnection(connectionString);
+        //UC-1 Check connection
+        public void EstablishConnection()
+        {
+            if (connection != null && connection.State.Equals(ConnectionState.Closed))
+                {
+                try
+                {
+                    connection.Open();                    
+                }
+                catch (Exception)
+                {
+                    throw new EmployeeException(EmployeeException.ExceptionType.Connection_Failed, "Connection failed..");
+                }
+            }           
+                if (connection != null && connection.State.Equals(ConnectionState.Open))
+                {
+                    try
+                    {
+                        connection.Close();                        
+                    }
+                    catch (Exception)
+                    {
+                        throw new EmployeeException(EmployeeException.ExceptionType.Connection_Failed, "Connection failed..");
+                    }
+                }            
+        }
+
+        static void Main(string[]args)
+        {
+            EmployeePayroll employeePayroll= new EmployeePayroll();
+            employeePayroll.EstablishConnection();
+        }
+    }
+
+
+}
