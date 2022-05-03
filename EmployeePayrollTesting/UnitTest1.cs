1ad51f2e49ec0891823c87b10653e77947d2a9ad
mythili annamalai <mythiliannamalai45@gmail.com>
2022-05-03 11:32:04 +0530

    salary testing

---
+++ EmployeePayrollTesting/UnitTest1.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using EmployeePayrollService;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+namespace EmployeePayrollTesting
+{
+    public class Tests
+    {
+        EmployeePayroll employeePayroll;
+        Employee employee;
+        [SetUp]
+        public void Setup()
+        {
+            employeePayroll = new EmployeePayroll();
+            employee = new Employee();
+        }
+        //UC-3 && 4  update salary testing
+        [Test]
+        public void UpdateSalary_Testing()
+        {    
+            Employee employee = new Employee();
+            employee.Emp_Id = 6;
+            employee.Emp_Name = "Terissa";
+            employee.Salary = 300000;
+            var res = EmployeePayroll.Updata_Salary(employee);
+            Assert.AreEqual(employee.Emp_Name,res.Emp_Name);
+        }        
+    }
+}
